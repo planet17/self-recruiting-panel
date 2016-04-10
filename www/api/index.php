e53f8fc22816ef779202cf9cc7018d9ce1126ace
planet17 <7.fast.cookies@gmail.com>
2016-04-10 22:11:03 +0300

    UPDATE WEB_ROOT

--- www/api/index.php
+++ www/api/index.php
@@ -1,10 +1,10 @@
 <?php
-$pathHome = __DIR__ . '/../home';
+$pathHome = __DIR__ . '/../../home';
 // comment out the following two lines when deployed to production
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 require($pathHome . '/myLittleHelper/dir.php');
 require($pathHome . '/yii2/vendor/autoload.php');
 require($pathHome . '/yii2/vendor/yiisoft/yii2/Yii.php');
-$config = require($pathHome . '/apps/web/config/main.php');
+$config = require($pathHome . '/apps/rest/config/main.php');
 (new yii\web\Application($config))->run();
