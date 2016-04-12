072199983d626eb3f748ffba84b880f8a95d832e
planet17 <7.fast.cookies@gmail.com>
2016-04-13 01:13:29 +0300

    Configuration set-up and FIX console-application

--- home/apps/common/config/aliases.php
+++ home/apps/common/config/aliases.php
@@ -4,15 +4,6 @@
  * Date: 26.02.16
  * Time: 0:06
  */
-/** TODO NEED FULL TEST AND REWRITE!!! DON'T USE THAT */
-/** Path to @apps directory */
-Yii::setAlias('@path-apps', helpers_pl17_dir_get(__DIR__, 3));
-
-/** Path to @common_apps_directory */
-Yii::setAlias('@path-common', Yii::getAlias('@path-apps') . DIRECTORY_SEPARATOR . 'common');
-Yii::setAlias('@path-common-config', Yii::getAlias('@path-common') . DIRECTORY_SEPARATOR . 'config');
-
-/** Path to @root_directory of website */
-Yii::setAlias('@path-www', helpers_pl17_dir_get(__DIR__, 5,['www']));
-Yii::setAlias('@link-www-common', '/common');
-Yii::setAlias('@path-www-common', Yii::getAlias('@path-www') . DIRECTORY_SEPARATOR . 'common');
+/** Set a GLOBAL alias @root_namespace for apps */
+Yii::setAlias('@apps', helpers_pl17_dir_get(__DIR__, 2));
+Yii::setAlias('@path-www', helpers_pl17_dir_get(__DIR__, 4,['www']));
--- home/apps/common/config/console.php
+++ home/apps/common/config/console.php
@@ -1,14 +1,14 @@
-<?php
+<?php require_once( __DIR__ . DIRECTORY_SEPARATOR . 'aliases.php');
 $db = true ?
     array_merge(require(__DIR__ . '/db-local.php')) :
     array_merge(require(__DIR__ . '/db-server.php'));
-//$params = require(__DIR__ . '/params.php');
+/*$params = require(__DIR__ . '/params.php');*/
 
 return [
-    'id' => 'minimal-console',
+    'id' => 'srp-console',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
-    'controllerNamespace' => 'app\commands',
+    'controllerNamespace' => 'apps\\common\\commands',
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
